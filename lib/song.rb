6fadd98ea8e3d1fd18610b0ef0d3f829b21a63fd
Michael Calkins <m.calkins576@gmail.com>
2019-11-18 22:01:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
   
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[0])
-    song.name = filename.split(" - ")[1]
+    song.artist = filename.split(" - ")[1]
     song
   end
   
