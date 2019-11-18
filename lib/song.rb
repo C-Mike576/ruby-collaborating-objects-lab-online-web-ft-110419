b7b288c28a1c63ac4efc6aac524b18c0fa1eefc3
Michael Calkins <m.calkins576@gmail.com>
2019-11-18 22:00:23 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+require './lib/artist.rb'
+
+class Song 
+  attr_accessor :name
+  attr_reader :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def artist=(artist_arg)
+    @artist = artist_arg
+  end
+  
+  def self.new_by_filename(filename)
+    song = self.new(filename.split(" - ")[0])
+    song.name = filename.split(" - ")[1]
+    song
+  end
+  
+  def artist_name=(name)
+    
+  end
+  
+  
+   
+end
